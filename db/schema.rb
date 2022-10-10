4149a4f8c3c2d950ff6b0c722fee76ee0112c251
Helio Oliveira <hheelliioo@live.com>
2022-10-10 19:42:21 -0300

    feat: add validations for username

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_210420) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  test "User must have a valid fixture" do
+  test "must have a valid fixture" do
     assert users(:one).valid?
+  end
+
+  test "validate presence of username" do
+    user = User.new
+
+    refute user.valid?
+    assert_equal user.errors.full_messages, ["Username can't be blank"]
+  end
+
+  test "validate uniqueness of username" do
+    user = User.new(username: users(:one).username)
+
+    refute user.valid?
+    assert_equal user.errors.full_messages, ["Username has already been taken"]
   end
 end
