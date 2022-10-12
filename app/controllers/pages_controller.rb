8f0fdd624723dc678d42511812b087fd5ccb5c34
Helio Oliveira <hheelliioo@live.com>
2022-10-12 10:58:26 -0300

    feat: add list views  for todos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
+    @users = User.pluck(:username, :id)
+    @active_todos = Todo.active
+    @completed_todos = Todo.completed
   end
 end
