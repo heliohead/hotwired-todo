4d39be020d3c67452832761f9b36224ce3fb0ee4
Helio Oliveira <hheelliioo@live.com>
2022-10-12 18:31:25 -0300

    feat: fix user creation test

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -4,7 +4,7 @@ class UsersTest < ApplicationSystemTestCase
   test "create new user and show success message" do
     visit root_url
     find("#user_username").set("New user")
-    click_button(class: "submit")
+    click_button(id: "user-submit")
 
     assert page.has_content?("User was successfully created.")
     refute page.has_content?("Username has already been taken")
@@ -13,7 +13,7 @@ class UsersTest < ApplicationSystemTestCase
   test "error when username already taken" do
     visit root_url
     find("#user_username").set(users(:one).username)
-    click_button(class: "submit")
+    click_button(id: "user-submit")
 
     assert page.has_content?("Username has already been taken")
     refute page.has_content?("User was successfully created.")
@@ -21,7 +21,7 @@ class UsersTest < ApplicationSystemTestCase
 
   test "error when username is empty" do
     visit root_url
-    click_button(class: "submit")
+    click_button(id: "user-submit")
 
     assert page.has_content?("Username can't be blank")
     refute page.has_content?("User was successfully created.")
