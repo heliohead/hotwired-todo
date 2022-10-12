deb12332f6bfad8222c6c17c167eff224df41389
Helio Oliveira <hheelliioo@live.com>
2022-10-12 15:54:22 -0300

    feat: validate presence of title

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,6 @@ class Todo < ApplicationRecord
   belongs_to :user
 
   enum status: {active: 0, completed: 1}
+
+  validates_presence_of :title
 end
--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -8,4 +8,11 @@ class TodoTest < ActiveSupport::TestCase
   test "must belongs_to user" do
     assert_equal Todo.reflect_on_association(:user).macro, :belongs_to
   end
+
+  test "validate presence of title" do
+    todo = Todo.new(user: users(:one))
+
+    refute todo.valid?
+    assert_equal todo.errors.full_messages, ["Title can't be blank"]
+  end
 end
